cfa609b815bb0b4d0566944bd87a12884fdda180
edwardmoon <jmoon0227@gmail.com>
2022-02-13 17:06:35 +0900

    make table:Bookmark

--- ch99/bookmark/admin.py
+++ ch99/bookmark/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from bookmark.models import Bookmark
 
-# Register your models here.
+@admin.register(Bookmark) # 데코레이터 사용하여 admin 사이트에 등록
+class BookmarkAdmin(admin.ModelAdmin):
+    '''Bookmark 클래스가 Admin 사이트에서 어떻게 보일지 정의'''
+    list_display = ('id', 'title', 'url')
--- ch99/bookmark/models.py
+++ ch99/bookmark/models.py
@@ -1,3 +1,16 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+
+# 테이블을 하나의 클래스로 정의하고, 테이블의 컬럼은 클래스의 변수로 매핑
+# 테이블 클래스는 django.db.models.Model 클래스를 상속받아 정의
+
+class Bookmark(models.Model):
+    title = models.CharField('TITLE', max_length=100, blank=True)
+    url = models.URLField('URL', unique=True)
+
+    def __str__(self):
+        return self.title
+
+
