c657350370ca80895b73a568043373d0d0a31d66
edwardmoon <jmoon0227@gmail.com>
2022-04-15 22:35:55 +0900

    ~view.py LV, DV

--- myweb/bookmark/models.py
+++ myweb/bookmark/models.py
@@ -1,13 +1,11 @@
-from pickle import _BufferCallback
 from pyexpat import model
-from turtle import title
 from django.db import models
-
-# Create your models here.
 
 class Bookmark(models.Model):
     title = models.CharField('TITLE', max_length=200, blank=True)
-    url = models.URLField('URL', unique=True) # URL == verbose_name(별칭)
+    url = models.URLField('URL', unique=True)
 
     def __str__(self) -> str:
         return self.title
+
+    
